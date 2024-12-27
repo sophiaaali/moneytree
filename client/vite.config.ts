ca2231ed38e332176960bbf568dd25968568d557
Sophia Li <yuan_li@brown.edu>
2024-12-27 05:53:38 +0400

    feat: optimize images for vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -13,14 +14,19 @@ export default defineConfig(({ mode }) => {
       "process.env.AUTH_DOMAIN": JSON.stringify(env.AUTH_DOMAIN),
       "process.env.PROJECT_ID": JSON.stringify(env.PROJECT_ID),
       "process.env.STORAGE_BUCKET": JSON.stringify(env.STORAGE_BUCKET),
-      "process.env.MESSAGING_SENDER_ID": JSON.stringify(
-        env.MESSAGING_SENDER_ID
-      ),
+      "process.env.MESSAGING_SENDER_ID": JSON.stringify(env.MESSAGING_SENDER_ID),
       "process.env.APP_ID": JSON.stringify(env.APP_ID),
       "process.env.MEASUREMENT_ID": JSON.stringify(env.MEASUREMENT_ID),
       "process.env.MAPBOX_TOKEN": JSON.stringify(env.MAPBOX_TOKEN),
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      viteImagemin({
+        pngquant: {
+          quality: [0.6, 0.8], // to speed up vercel deployment
+        },
+      }),
+    ],
     server: {
       port: 8000,
     },
