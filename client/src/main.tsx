df9cc347ac4f2e4370ec0a8f7630b37d9e7a5208
Sophia Li <yuan_li@brown.edu>
2024-12-27 08:29:18 +0400

    fix: home page animation display

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
-// import "./styles/index.css";
 import App from "./components/App";
 import { ClerkProvider } from "@clerk/clerk-react";
 
